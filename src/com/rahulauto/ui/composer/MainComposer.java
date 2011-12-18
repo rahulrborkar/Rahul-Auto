4c91624b7803796f0f9fe23fdd23cc63eb9fe01c
Prasanna Sherekar <prasanna@localhost.localdomain>
2011-12-19 01:39:34 +0530

    UI Modified - ListBox is replaced by grid

--- src/com/rahulauto/ui/composer/MainComposer.java
+++ src/com/rahulauto/ui/composer/MainComposer.java
@@ -1,23 +1,24 @@
 package com.rahulauto.ui.composer;
 
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.Path;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Include;
 
 public class MainComposer extends GenericForwardComposer {
-	
+
+	private static final long serialVersionUID = 72604379476908925L;
+
 	private Include contents;
-	public void doAfterCompose(Component comp) throws Exception {
-        super.doAfterCompose(comp);
-	}
 	
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+	}
+
 	private void displayPage(String sourceZul){		
-		contents.setSrc(null);
 		contents.setSrc(sourceZul);
 	}
-	
+
 	public void onClick$listPart() {
 		displayPage("/admin/part/partList.zul");
-    }
+	}
 }
--- src/com/rahulauto/ui/composer/PartListController.java
+++ src/com/rahulauto/ui/composer/PartListController.java
@@ -4,12 +4,13 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 
 public class PartListController extends GenericForwardComposer {
-	
+
+	private static final long serialVersionUID = -2635636472443599553L;
+
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
-		// TODO Auto-generated method stub
 		super.doAfterCompose(comp);
-		
-		
+
 	}
+
 }
